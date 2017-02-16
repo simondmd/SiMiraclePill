334da73547d639dd4a9b17b7ce40861ff304f107
simondmd <simondmd@hotmail.com>
2017-02-16 13:49:04 +0100

    buy button and success indicator

--- SiMiraclePill/ViewController.swift
+++ SiMiraclePill/ViewController.swift
@@ -22,6 +22,8 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     @IBOutlet weak var countryTxtField: UITextField!
     @IBOutlet weak var zipLabel: UILabel!
     @IBOutlet weak var zipTxtField: UITextField!
+    @IBOutlet weak var whiteCover: UIView!
+    @IBOutlet weak var successImage: UIImageView!
     
 //array of values for the state picker roladex
     let states = ["Alaska", "Alabama", "Arkansas", "American Samoa", "Arizona", "California", "Colorado", "Connecticut", "District of Columbia", "Delaware", "Florida", "Georgia", "Guam", "Hawaii", "Iowa", "Idaho", "Illinois", "Indiana", "Kansas", "Kentucky", "Louisiana", "Massachusetts", "Maryland", "Maine", "Michigan", "Minnesota", "Missouri", "Mississippi", "Montana", "North Carolina", "North Dakota", "Nebraska", "New Hampshire", "New Jersey", "New Mexico", "Nevada", "New York", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Puerto Rico", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Virginia", "Virgin Islands", "Vermont", "Washington", "Wisconsin", "West Virginia", "Wyoming"]
@@ -67,5 +69,10 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         zipLabel.isHidden = false
         zipTxtField.isHidden = false
     }
-}
+    
+    @IBAction func buyBtnPressed(_ sender: Any) {
+        whiteCover.isHidden = false
+        successImage.isHidden = false
+    }
 
+}
